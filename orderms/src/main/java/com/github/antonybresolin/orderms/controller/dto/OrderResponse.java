c06cf1949fe4b0c42323d167f411e0b18267227d
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-07-31 16:59:32 -0300

    fix: error main branch

--- orderms/src/main/java/com/github/antonybresolin/orderms/controller/dto/OrderResponse.java
+++ orderms/src/main/java/com/github/antonybresolin/orderms/controller/dto/OrderResponse.java
@@ -4,5 +4,5 @@ import java.math.BigDecimal;
 
 public record OrderResponse(Long orderId,
                             Long customerId,
-                            BigDecimal total,
+                            BigDecimal total) {
 }
